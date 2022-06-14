236d559d8917cf55cce0b5ad9a2c66d607bc05b7
moises-ph <64561711+moises-ph@users.noreply.github.com>
2022-06-14 10:05:14 -0500

    Delete prueba.js

--- prueba.js
+++
@@ -1,8 +0,0 @@
-var req = new XMLHttpRequest;
-var DATA = {};
-req.open('GET', 'http://localhost:3000/dashboard/data', false);
-req.send(null);
-if (req.status == 200) {
-    DATA = JSON.parse(req.responseText);
-    console.log(DATA)
-}
